3f54feaf2f288e4e66cd6b561065b93e0f697e02
Jonathan Aerts <jonathan.aerts@kissho-academy.com>
2025-09-11 09:35:41 +0200

    ffff

--- ResourceGroup/main.tf
+++ ResourceGroup/main.tf
@@ -21,19 +21,11 @@ resource "azurerm_resource_group" "this" {
 }
 
 # Optionally create a management lock on the resource group to prevent deletion.
-resource "azurerm_management_lock" "rg_lock" {
-  # Lock name includes the resource group name.
-  name = "lock-${var.name}"
+resource "azurerm_management_lock" "this" {
+  count = var.lock != null ? 1 : 0
 
-  # Scope of the lock is the resource group.
-  scope = azurerm_resource_group.this.id
-
-  # Lock level set to 'CanNotDelete' to prevent accidental deletion.
-  lock_level = "CanNotDelete"
-
-  # Ensure the lock is created after the resource group.
-  depends_on = [azurerm_resource_group.this]
-
-  # Only create the lock if 'enable_lock' variable is true.
-  count = var.lock ? 1 : 0
+  lock_level = var.lock.kind
+  name       = coalesce(var.lock.name, "lock-${var.lock.kind}")
+  scope      = azurerm_resource_group.this.id
+  notes      = var.lock.kind == "CanNotDelete" ? "Cannot delete the resource or its child resources." : "Cannot delete or modify the resource or its child resources."
 }
